12069307e8be54279d56a1715fbb22d5cb5c6784
Aykut <aykutnazifov@gmail.com>
2023-01-29 21:52:06 +0200

    change connectdb

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -1,40 +1,21 @@
+/* This is a database connection function*/
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI;
-
-if (!MONGO_URI) {
-  throw new Error(
-    "Please define the MONGO_URL environment variable inside .env.local"
-  );
-}
-
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+const connection = {}; /* creating connection object*/
 
 async function connectDb() {
-  if (cached.conn) {
-    return cached.conn;
+  /* check if we have connection to our databse*/
+  if (connection.isConnected) {
+    return;
   }
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
+  /* connecting to our database */
+  const db = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-    cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
+  connection.isConnected = db.connections[0].readyState;
 }
 
 export default connectDb;
